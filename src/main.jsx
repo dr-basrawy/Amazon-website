c44d9f70f1e0b74376bef8e251da06e16282b73e
Dr.Basrawy <dr.basrawy12@gmail.com>
2024-02-06 00:03:13 +0200

    css fx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './i18n';
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+import '../node_modules/react-rating/lib/react-rating.esm.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
